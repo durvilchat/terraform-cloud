2f0b1bfb0f8bd70f3c0f35cb12c3670b2e320689
durvilchat <rostapig@gmail.com>
2024-08-27 22:03:05 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -10,3 +10,12 @@ resource "aws_instance" "myec2" {
     Name = "ec2-durvil"
   }
 }
+
+resource "aws_instance" "server" {
+  ami           = "ami-07e35c3920b92d884"
+  instance_type = "t2.micro"
+  key_name      = "devops-durvil"
+  tags = {
+    Name = "ec2-server"
+  }
+}
